cd12946951f5494854efb79801cfaafa5283df4f
Princeton J. Rose <princetonjrose@gmail.com>
2022-12-28 09:57:13 -0500

    finished lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,14 @@
 
 def print_fibonacci(length):
     pass
+    if length <= 0:
+        print( [] )
+    elif length == 1:
+        print( [0] )
+    elif length == 2:
+        print( [0,1] )
+    else:
+        fib = [0,1]
+        for num in range( 2, length ):
+            fib.append( fib[-2] + fib[-1] )
+        print( fib )
